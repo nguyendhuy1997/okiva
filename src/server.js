859e1ba93a3dc70929304c0c75f6c2cc77c15e9b
nguyendhuy1997@gmail.com <nguyendhuy1997@gmail.com>
2019-12-17 10:27:18 +0700

    update deploy angular

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/okiva-demo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/okiva-demo>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/okiva-demo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
